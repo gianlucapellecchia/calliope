4b8869efb5e0abcd09897ec5b41204b9ee5c62cd
brynpickering <bryn.pickering@usys.ethz.ch>
2020-04-29 09:55:41 +0100

    Move and update rule files

--- calliope/config/rules/chicken_dinner.py
+++ calliope/config/rules/chicken_dinner.py
@@ -2,40 +2,32 @@ from calliope import __version__ as CALLIOPE_VERSION
 
 from typing import Callable, Dict, Sequence, get_type_hints, TypeVar
 
-from pydantic import validator
-from pydantic.types import datetime
+from pydantic import validator, ValidationError
+from datetime import datetime
+from dateutil import parser
 
-__all__ = []
+
+__all__ = ["range_check", "objective_options_validation", "calliope_version_validation", "timeseries_dateformat_validation"]
 
 Comparable_t = TypeVar("Comparable_t", int, float, datetime)
 
 # TODO: rewrite as a simple function, where the parameters are injected into
 # the closure in a generic factory method
-def range_check(key, keys, **opts):
-    @validator(key, **opts)
-    def _range_check(cls, _max, values):
-        """Validator function for a range config object"""
-        if keys[0] in values and values[keys[0]] > _max:
-            raise ValueError(f"bad range: {values[keys[0]]} > {_max}")
-        return _max
-
-    return {"range_check": _range_check}
+def range_check(cls, _max, values, *, min_key):
+    """Validator function for a range config object"""
+    if min_key in values and values[min_key] > _max:
+        raise ValueError(f"bad range: {values[min_key]} > {_max}")
+    return _max
 
 
-# def objective_options_validation(
-#     key: str, keys: Sequence[str], **opts
-# ) -> Dict[str, classmethod]:
-#     @validator(key, **opts)
-#     def _objective_options_validation(cls, _objective, values):
-#         """
-#         'cost_class' and 'sense' need to be defined if run.objective is 'minmax_cost_optimisation'
-#         """
-#         if _objective == 'minmax_cost_optimization':
-#             if "cost_class" not in values or "sense" not in values:
-#                 return ValueError("minmax_cost_optimisation chosen with either cost_class or sense undefined")
-#         return _objective
-
-#     return {"objective_options_validation", _objective_options_validation}
+def objective_options_validation(cls, _objective, values):
+    """
+    'cost_class' and 'sense' need to be defined if run.objective is 'minmax_cost_optimisation'
+    """
+    if _objective == 'minmax_cost_optimization':
+        if "cost_class" not in values or "sense" not in values:
+            return ValueError("minmax_cost_optimisation chosen with either cost_class or sense undefined")
+    return _objective
 
 
 # def operate_options_validation():
@@ -57,24 +49,14 @@ def range_check(key, keys, **opts):
 #         }
 #         if keys[0] in values and values[keys[0]] == 'pyomo' and not _backend_options.keys
 
-
-def calliope_version_validation(
-    key: str, keys: Sequence[str], **opts
-) -> Dict[str, classmethod]:
-    """
-    Given version should match match calliope.__version__.
-    """
-
-    @validator(key, **opts)
-    def _calliope_version_validation(cls, _version: str, values):
-        """Validator function for Calliope version"""
-        if _version != CALLIOPE_VERSION:
-            raise ValueError(
-                f"Calliope version mismatch: model specifies {_version} but running {CALLIOPE_VERSION}"
-            )
+def calliope_version_validation(cls, _version, values):
+    """Validator function for a range config object"""
+    if _version != CALLIOPE_VERSION:
+        raise ValueError(
+            f"Calliope version mismatch: model specifies {_version} but running {CALLIOPE_VERSION}"
+        )
+    else:
         return _version
-
-    return {"calliope_version_validation": _calliope_version_validation}
 
 
 # def mask_validation(
@@ -104,20 +86,25 @@ def calliope_version_validation(
 #    """
 
 
-def timeseries_data_validation():
-    """
-    string:pandas.DataFrame pairs, where string must be one of the 'file=...' strings found in the data. 
-    NOTE: we'll probably  get rid of this configuration as it is simply exposing implementation to the user
-    """
+# def timeseries_data_validation():
+#     """
+#     string:pandas.DataFrame pairs, where string must be one of the 'file=...' strings found in the data.
+#     NOTE: we'll probably  get rid of this configuration as it is simply exposing implementation to the user
+#     """
 
 
-def timeseries_dateformat_validation():
+def timeseries_dateformat_validation(cls, _dateformat, values):
     """
     This string must be a valid datetime format string, e.g. '%Y-%m-%d %H:%M:%S'
     """
+    try:
+        parser.parse(datetime.now.strftime(_dateformat))
+        return _dateformat
+    except parser.ParserError:
+        raise ValidationError("Unknown format codes used in timeseries date format")
 
 
-def validate_clustering_consistency():
+# def validate_clustering_consistency():
     # # Don't allow time clustering with cyclic storage if not also using
     # # storage_inter_cluster
     # storage_inter_cluster = "model.time.function_options.storage_inter_cluster"
