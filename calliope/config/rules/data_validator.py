c6ee0756d1f20507c920b44cbec9d69204393cff
brynpickering <bryn.pickering@usys.ethz.ch>
2020-11-09 13:40:23 +0100

    Add validators (2020-11 code sprint AM)

--- calliope/config/rules/data_validator.py
+++ calliope/config/rules/data_validator.py
@@ -1,62 +1,63 @@
+def inheritance(cls, val, values, *, allowed_in):
+    # if issubclass(cls, allowed_in):  # FIXME: get the types
+    #     raise ValueError(f"{cls} does not inherit from either of {allowed_in}")
 
-def allowed_in_validation(
-    key: str, keys: Sequence[str], validator_params, **opts
-) -> Dict[str, classmethod]:
+    # MRO ends in the base property class, and object, remove those
+    if any(
+        map(
+            lambda i: i[0] in i[1],
+            product(allowed_in, map(str, cls.mro()[:-2])),  # FIXME: nasty hack
+        )
+    ):
+        return val
+    raise TypeError(f"{cls} does not inherit from either of {allowed_in}")
+
+
+def carrier_validation(cls, val, values, *, direction):
+    defined_carriers = [v for k, v in values.items() if f"_{direction}_" in k]
+    if val in defined_carriers:
+        return val
+    else:
+        raise ValueError(f"Primary carrier {direction} {val} not defined as a carrier for this technology")  # FIXME: do we know what the tech name is?
+
+
+def not_resource(cls, val, values, *, excluded):
     """
-    Check if a setting is allowed for this tech group.
+    `resource` is special, you can't call a carrier that.
     """
-
-    @validator(key, **opts)
-    def _allowed_in_validation(cls, _setting: str, values):
-        """Validator function for tech-group allowed settings."""
-        if 
-            raise ValueError(
-                f"Calliope version mismatch: model specifies {_version} but running {CALLIOPE_VERSION}"
-            )
-        return _version
-
-    return {"allowed_in_validation": _allowed_in_validation}
+    defined_exclusions = set(val).intersection(excluded)
+    if defined_exclusions:
+        raise ValueError(f"Cannot define the carrier(s) {defined_exclusions} within {cls}")
+    return val
 
 
 def cost_class_validation()
     """
-    Look back at config and check that the cost classes defined in the objective 
+    Look back at config and check that the cost classes defined in the objective
     options match those defined as cost classes in the data.
     NOTE: this should cover all instances of CostClassDict!
     """
 
-def node_coordinate_validation():
+def node_coordinate_validation(cls, val, values, *, coordinate_systems):
     """
     Can only define [x, y] or [lat, lon], no other combination of keys, e.g. [x, y, lat, lon], [x, lat]
-    and all locations must be consistent, all [x, y] or all [lat, lon]
     """
+    # TODO: also validate "all locations must be consistent, all [x, y] or all [lat, lon]" in another validation step
+    coordinate_systems = [set("x", "y"), set("lat", "lon")]
+
+    coord_sys = set(val.keys())
+    if coord_sys in coordinate_systems:
+        return val
+    else:
+        raise ValueError("Can only define [x, y] or [lat, lon], no other combination of keys, "
+                         "e.g. [x, y, lat, lon], [x, lat]")
 
 
 def node_validation():
     """
-    If 'techs' key defined, 'transmission_node' cannot be set to True. Although 
-    'transmission_node' is somewhat exposing implementation to the user, and 
+    If 'techs' key defined, 'transmission_node' cannot be set to True. Although
+    'transmission_node' is somewhat exposing implementation to the user, and
     could possibly be removed entirely
-    """
-
-
-def carrier_key_choice_validation():
-    """
-    NOTE: this is dealt with now by 'allowed_in'
-    The allowed carrier keys that can be used depends on the top-level tech_group
-    associated with each tech's parent.
-    """
-
-
-def carrier_validation(direction):
-    """
-    String must match one of the carriers defined under [{direction}, {direction}_2, {direction}_3]
-    for this tech
-    """
-
-def no_carrier_may_be_called_resource():
-    """
-    `resource` is special, you can't call a carrier that.
     """
 
 def error_if_a_technology_is_defined_twice_in_opposite_directions():
@@ -80,42 +81,73 @@ def error_if_a_technology_is_defined_twice_in_opposite_directions():
 
 #     return {"objective_options_validation", _objective_options_validation}
 
-def export_cost_validation(
-    key, keys, **opts
-):
-    @validator(key, **opts)
-    def _export_cost_validation(cls, _export, values):
-        """
-        Can only define an export cost if an export carrier has been defined for the same technology
-        """
-        if 
+def export_cost_validation():
+    """
+    Can only define an export cost if an export carrier has been defined for the same technology
+    """
+
+def energy_cap_per_unit(cls, val, values, *):
+    """
+    If units are defined, then energy_cap_per_unit must be specified.
+    """
+    dependent_val = values.get('energy_cap_per_unit', None)
+    if dependent_val is not None:
+        return val
+    else:
+        raise ValueError("If units are defined, then energy_cap_per_unit must be specified.")
+
+def storage_cap_per_unit(cls, val, values, *):
+    """
+    If units are defined (and storage is in the inheritance...), then storage_cap_per_unit must be specified.
+    """
+    dependent_val = values.get('storage_cap_per_unit', None)
+    storage_in_hierarchy = False  # TODO: Fixme, is a placeholder now
+    if dependent_val is not None and storage_in_hierarchy:
+        return val
+    else:
+        raise ValueError("If units are defined (and storage is in the inheritance...), "
+                         "then storage_cap_per_unit must be specified.")
+
+def finite_forced_resource(cls, val, values, *):
+    """
+    If a resource value is forced, this value cannot be infinite.
+    """
+    resource_val = values.get('resource', None)
+    if np.isinf(resource_val):
+        raise ValueError("Can't be infinte")
+    elif isinstance(resource_val, str):
+        resource_ts_val = get_timeseries_data(resource_val)
+        # TODO: get_timeseries_data should be a key in timeseries_dataframes dictionary
+        if resource_ts_val.isinf().any():
+            raise ValueError("Can't be infinte")
+    else:
+        return val
 
 
-def energy_cap_per_unit_requires_units_to_be_specified():
-
-def storage_cap_per_unit_requires_units_to_be_specified():
-
-def force_resource_requires_finite_resource():
-
-def warn_if_group_constraint_contains_no_valid_loctechs():
-
-def warn_if_loctech_is_defined_in_group_constraint_but_not_in_model():
-
-def storage_initial_being_gte_to_storage_discharge_depth():
-
+def gte_storage_discharge_depth(cls, val, values, *):
+    """
+    If defining both `storage_initial` and `storage_discharge_depth`
+    then `storage_initial` >= `storage_discharge_depth`
+    """
+    storage_dod_val = values.get('storage_discharge_depth', 0)
+    if val >= storage_dod_val:
+        return val
+    else:
+        raise ValueError("""If defining both `storage_initial` and `storage_discharge_depth` then `storage_initial` >= `storage_discharge_depth`""")
 
 ##
 # Timeseries specific things follow below
 ##
 
 def loc_tech_cannot_have_force_resource_and_infinite_resource():
+    pass
 
 def ensure_that_if_a_tech_has_negative_costs_there_is_a_max_cap_defined():
     caps = ["energy_cap", "storage_cap", "resource_cap", "resource_area"]
     costs = ["cost_" + i in model_data.data_vars.keys()]
 
 def demand_only_allowed_negative_resource():
+    pass
 
 def supply_only_allowed_positive_resource():
-
-
+    pass
