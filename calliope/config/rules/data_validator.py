ba5bdb032529f8922676c010a9a39de38976f89d
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2020-11-10 17:20:25 +0100

    rules: validator update from code sprint
    - mostly non-working pseudo-code

--- calliope/config/rules/data_validator.py
+++ calliope/config/rules/data_validator.py
@@ -1,3 +1,6 @@
+from itertools import product
+
+
 def inheritance(cls, val, values, *, allowed_in):
     # if issubclass(cls, allowed_in):  # FIXME: get the types
     #     raise ValueError(f"{cls} does not inherit from either of {allowed_in}")
@@ -18,8 +21,10 @@ def carrier_validation(cls, val, values, *, direction):
     if val in defined_carriers:
         return val
     else:
-        raise ValueError(f"Primary carrier {direction} {val} not defined as a carrier for this technology")  # FIXME: do we know what the tech name is?
-
+        # raise ValueError(f"Primary carrier {direction} {val} not defined as a carrier for this technology")  # FIXME: do we know what the tech name is?
+        raise ValueError(f"`{tech}` at `{loc}` is attempting to export a carrier "
+                "not given as an output carrier: `{export}`")
+                # see checks.py:530
 
 def not_resource(cls, val, values, *, excluded):
     """
@@ -31,7 +36,7 @@ def not_resource(cls, val, values, *, excluded):
     return val
 
 
-def cost_class_validation()
+def cost_class_validation():
     """
     Look back at config and check that the cost classes defined in the objective
     options match those defined as cost classes in the data.
@@ -39,18 +44,19 @@ def cost_class_validation()
     """
 
 def node_coordinate_validation(cls, val, values, *, coordinate_systems):
+    """Can only define [x, y] or [lat, lon], no other combination of keys,
+    e.g. [x, y, lat, lon], [x, lat]
+
     """
-    Can only define [x, y] or [lat, lon], no other combination of keys, e.g. [x, y, lat, lon], [x, lat]
-    """
-    # TODO: also validate "all locations must be consistent, all [x, y] or all [lat, lon]" in another validation step
-    coordinate_systems = [set("x", "y"), set("lat", "lon")]
+    # TODO: also validate "all locations must be consistent, all [x, y] or all
+    # [lat, lon]" in another validation step
+    coordinate_systems = [{"x", "y"}, {"lat", "lon"}]
 
     coord_sys = set(val.keys())
     if coord_sys in coordinate_systems:
         return val
     else:
-        raise ValueError("Can only define [x, y] or [lat, lon], no other combination of keys, "
-                         "e.g. [x, y, lat, lon], [x, lat]")
+        raise ValueError("Incompatible coordinate systems: allowed combinations {x, y} or {lat, lon}")
 
 
 def node_validation():
@@ -64,7 +70,6 @@ def error_if_a_technology_is_defined_twice_in_opposite_directions():
     """
     What the function name says.
     """
-
 
 # def objective_options_validation(
 #     key: str, keys: Sequence[str], **opts
@@ -86,7 +91,7 @@ def export_cost_validation():
     Can only define an export cost if an export carrier has been defined for the same technology
     """
 
-def energy_cap_per_unit(cls, val, values, *):
+def energy_cap_per_unit(cls, val, values, /):
     """
     If units are defined, then energy_cap_per_unit must be specified.
     """
@@ -96,7 +101,7 @@ def energy_cap_per_unit(cls, val, values, *):
     else:
         raise ValueError("If units are defined, then energy_cap_per_unit must be specified.")
 
-def storage_cap_per_unit(cls, val, values, *):
+def storage_cap_per_unit(cls, val, values, /):
     """
     If units are defined (and storage is in the inheritance...), then storage_cap_per_unit must be specified.
     """
@@ -108,7 +113,7 @@ def storage_cap_per_unit(cls, val, values, *):
         raise ValueError("If units are defined (and storage is in the inheritance...), "
                          "then storage_cap_per_unit must be specified.")
 
-def finite_forced_resource(cls, val, values, *):
+def finite_forced_resource(cls, val, values, /):
     """
     If a resource value is forced, this value cannot be infinite.
     """
@@ -124,7 +129,7 @@ def finite_forced_resource(cls, val, values, *):
         return val
 
 
-def gte_storage_discharge_depth(cls, val, values, *):
+def gte_storage_discharge_depth(cls, val, values, /):
     """
     If defining both `storage_initial` and `storage_discharge_depth`
     then `storage_initial` >= `storage_discharge_depth`
@@ -140,14 +145,55 @@ def gte_storage_discharge_depth(cls, val, values, *):
 ##
 
 def loc_tech_cannot_have_force_resource_and_infinite_resource():
+    # TODO: Probably not needed, covered by finite_forced_resource
     pass
 
 def ensure_that_if_a_tech_has_negative_costs_there_is_a_max_cap_defined():
     caps = ["energy_cap", "storage_cap", "resource_cap", "resource_area"]
+    # if the costs for the above is negative, then ensure there is a *_max
+    # if costs.*.$CAPS for $CAPS in caps (list above) is negative, then also
+    # constraints.$CAPS_max need to be specified and finite
     costs = ["cost_" + i in model_data.data_vars.keys()]
 
-def demand_only_allowed_negative_resource():
-    pass
+# for loc_techs_demand see preprocess/checks.py:840
+def check_resource_sign(cls, val, values, *):
+    resource_ts_val = get_timeseries_data(val) 
+    # TODO: get_timeseries_data should be a key in timeseries_dataframes dictionary
+    # demand is usually time series, but may also be fixed value
+    # check if inherits from demand or supply(-like) 
+    if cls.inherits_from(demand):
+        if (resource_ts_val > 0).any():
+            raise ValueError("Positive resource given for demand loc_tech {}. All demands "
+                    "must have negative resource".format(loc_tech)) # FIXME: 
+    else:   # inherits from supply
+        if (resource_ts_val < 0).any():
+            raise ValueError("Negative resource given for supply loc_tech {}. All resources "
+                    "must have positive resource".format(loc_tech))  # FIXME
+    return val
 
+# opposite logic as above
 def supply_only_allowed_positive_resource():
     pass
+
+def calliope_version_validator(cls, val, values, /):
+    if calliope.__version__ != val:
+        # currently raises a model warning, meaning you can still run, retain the behaviour
+        # unit tests often check for specific errors/warnings, ensure error message doesn't change
+        raise ValueError(f"Model configuration specifies calliope_version={val}, "
+                "but you are running {calliope.__verison__}. Proceed with caution!")
+    return val
+
+# this is a root validator to be applied at the technology definition level
+def contains_keys(cls, val, *, desired_keys):
+    # TODO: get techs with storage, assume it is in the following variable
+    # tech_w_storage = ("supply_plus", ) # defined in calliope.preprocess.sets.loc_techs_store
+    # if issubclass(cls, tech_w_storage):
+    all_keys = set(val.keys())
+    if any(i in desired_keys for i in all_keys):
+        return val
+    else:
+        # change to log, current implementation doesn't throw error
+        # loc + tech are part of the val name, so extract & split to match msg below
+        raise ValueError(f"`{tech}`` at `{loc}` has no constraint to explicitly connect `energy_cap` to "
+            "`storage_cap`, consider defining a `energy_cap_per_storage_cap_min/max/equals` "
+            "constraint")
